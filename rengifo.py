dc09cc34eef348ff6e3041d3d9947c948ac675af
Rengifoo0 <sebastianrengio0731@gmail.com>
2024-08-05 14:45:05 -0500

    mi primer ejercicio

---
+++ rengifo.py
@@ -0,0 +1,4 @@
+nombre=input("El estudiante: ")
+Colegio=input("nombre del colegio:")
+Curso=input("grado que cursa:")
+print(f"el estudiante {nombre} estudia en el {Colegio} el programa de formacion {Curso}")
